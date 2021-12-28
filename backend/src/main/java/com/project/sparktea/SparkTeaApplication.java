ed504f2562fd03265e443e234be2d337b3272efb
jeishu <jeremy.zhu@utexas.edu>
2021-12-28 15:36:05 -0600

    first commit

---
+++ backend/src/main/java/com/project/sparktea/SparkTeaApplication.java
@@ -0,0 +1,13 @@
+package com.project.sparktea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkTeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkTeaApplication.class, args);
+	}
+
+}
