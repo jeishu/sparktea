69bb300bba3e9917e2202e567cf3fd4067b30495
jeishu <jeremy.zhu@utexas.edu>
2021-12-31 15:16:58 -0600

    added a navbar with styling

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import {Link, Outlet} from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
+import "./nav.scss";
+import Dashboard from '../../img/nav/Dashboard';
+import Home from '../../img/nav/Home';
+import Profile from '../../img/nav/Profile';
+import Signout from '../../img/nav/Signout';
 
 function Navbar() {
     return (
-        <div>
-            <h1>Navbar</h1>
-            <Link to="/home">Home</Link> 
-            {" | "}
-            <Link to="/profile">profile</Link>
-            <hr/>
+        <div className='nav-bar'>
+            <Link to="/home"><Home/></Link> 
+            <Link to="/profile"><Profile/></Link>
+            <Link to="/signout"><Signout/></Link>
             <Outlet/>
         </div>
     )
