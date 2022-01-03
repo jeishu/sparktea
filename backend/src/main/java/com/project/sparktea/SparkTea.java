ca797d9de7df4d98e50eea46fda95a9ff389ad39
jeishu <jeremy.zhu@utexas.edu>
2022-01-03 16:37:58 -0600

    overhaul in our code

---
+++ backend/src/main/java/com/project/sparktea/SparkTea.java
@@ -0,0 +1,13 @@
+package com.project.sparktea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkTea {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkTea.class, args);
+	}
+
+}
