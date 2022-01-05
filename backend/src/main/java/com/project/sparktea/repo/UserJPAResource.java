398914ed68050a0823df57c38cd9b44785a94158
jeishu <jeremy.zhu@utexas.edu>
2022-01-05 10:00:02 -0600

    fixing errors

--- backend/src/main/java/com/project/sparktea/repo/UserJPAResource.java
+++ backend/src/main/java/com/project/sparktea/repo/UserJPAResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
+@CrossOrigin(origins="http://localhost:3000/")
 @RequestMapping("/users")
 public class UserJPAResource {
 
