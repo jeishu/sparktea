ce6ef56d362557d45804ec6e971ebfa08195b84c
jeishu <jeremy.zhu@utexas.edu>
2022-01-06 14:21:28 -0600

    deleted other files that were added for implementation

--- CaramelApple/src/main/java/com/caramel/apple/user/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.caramel.apple.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<Users, Integer> {
-
-}
