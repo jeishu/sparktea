23c9fffed9b1060c5d1d8815b82b041ecc11889f
jeishu <jeremy.zhu@utexas.edu>
2022-01-07 04:12:55 -0600

    added lots of features to the frontend

--- backend/src/main/java/com/project/sparktea/SparkTea.java
+++ backend/src/main/java/com/project/sparktea/SparkTea.java
@@ -2,7 +2,9 @@ package com.project.sparktea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan
 @SpringBootApplication
 public class SparkTea {
 
--- frontend/src/components/share/Share.jsx
+++ frontend/src/components/share/Share.jsx
@@ -1,18 +1,30 @@
 import "./share.css";
 import { useState } from "react";
-import axios from "axios"; 
+import axios from "axios";
+import moment from "moment";
 
 export default function Share() {
-  const [values,setValues] = useState({contnt:""});
-  // const [user,setUser] = useState({ })
-  // const [date,setDate] = useState({dat:""});
+  const [values, setValues] = useState({ contnt: "", dat: "" });
 
-  const handleTextChange= (event) => {setValues({...values,contnt: event.target.value})}
-  // const handleTextChange= (event) => {setValues({...date,dat: value.contnt, dat: date})}
-  
-  function registerTextData(){
-    axios.post("http://localhost:7070/posts/create",{contnt:values.contnt})
-    .then(response =>{console.log(response.status)}).catch(error => console.log(error))
+  let time = moment().format("MMM Do YYYY, h:mm a");
+  console.log(time)
+
+  const handleTextChange = (event) => {
+    setValues({...values, contnt: event.target.value, dat: time});
+    console.log(values)
+  }
+
+  const registerTextData = () => {
+    axios.post("http://localhost:7070/posts/create", {
+      contnt: values.contnt,
+      date: values.dat
+    }).then(response => {
+      console.log(response.status)
+      console.log(response.data)
+    }).catch(error => console.log(error))
+      
+    window.location.reload(true);
+
   }
 
   return (
@@ -27,13 +39,13 @@ export default function Share() {
             onChange={handleTextChange}
           />
         </div>
-        <hr className="shareHr"/>
+        <hr className="shareHr" />
         <div className="shareBottom">
-            <div className="shareOptions">
-                <div className="shareOption">
-                    <button onClick={registerTextData} className="shareButton">Spark</button>
-                </div>            
+          <div className="shareOptions">
+            <div className="shareOption">
+              <button onClick={registerTextData} className="shareButton">Spark</button>
             </div>
+          </div>
         </div>
       </div>
     </div>
